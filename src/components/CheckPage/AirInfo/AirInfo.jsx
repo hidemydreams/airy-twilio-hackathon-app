75810b9c17cae5b6c23f6fd958704af078ee901e
Nazar Archakov <nazar-archakou_student2021@wilder.school>
2021-03-12 23:21:24 +0300

    feat: add before-start block

--- src/components/CheckPage/AirInfo/AirInfo.jsx
+++ src/components/CheckPage/AirInfo/AirInfo.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import BeforeStart from '../BeforeStart/BeforeStart'
 
 export default function AirInfo({ airInfo }) {
+  const [info, setInfo] = useState(null)
 
+  useEffect(() => {
+    setInfo(airInfo)
+  }, [airInfo])
 
-  // console.log(airInfo.list[0].components)
   return (
-    <div className="card">
+    <div className="info-section">
       {airInfo.list ? (<ul>
-        <li>{airInfo.list[0].components.co}</li>
-        <li>{airInfo.list[0].components.no}</li>
-        <li>{airInfo.list[0].components.no2}</li>
-        <li>{airInfo.list[0].components.o3}</li>
-        <li>{airInfo.list[0].components.so2}</li>
-        <li>{airInfo.list[0].components.pm2_5}</li>
-        <li>{airInfo.list[0].components.nh3}</li>
-        <li>{airInfo.list[0].components.pm10}</li>
-      </ul>) : <p>Loading</p>}
+        <h1>{airInfo?.list[0].main.aqi}</h1>
+        <li><span>CO</span>: {airInfo.list[0].components.co}, μg/m<sup>3</sup></li>
+        <li><span>NO</span>:{airInfo.list[0].components.no}, μg/m<sup>3</sup></li>
+        <li><span>NO<sub>2</sub></span>:{airInfo.list[0].components.no2}, μg/m<sup>3</sup></li>
+        <li><span>O<sub>3</sub></span>:{airInfo.list[0].components.o3}, μg/m<sup>3</sup></li>
+        <li><span>SO<sub>2</sub></span>:{airInfo.list[0].components.so2}, μg/m<sup>3</sup></li>
+        <li><span>PM<sub>2.5</sub></span>:{airInfo.list[0].components.pm2_5}, μg/m<sup>3</sup></li>
+        <li><span>PM<sub>10</sub></span>:{airInfo.list[0].components.nh3}, μg/m<sup>3</sup></li>
+        <li><span>NH<sub>3</sub></span>:{airInfo.list[0].components.pm10}, μg/m<sup>3</sup></li>
+      </ul>) : <BeforeStart />}
     </div>
   )
 }
