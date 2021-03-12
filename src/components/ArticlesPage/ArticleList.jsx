6c67cc061a1eedebbdb9831ffb5bc8d9b2a35335
gortatka <gtatsiana@tut.by>
2021-03-12 23:13:06 +0300

    add ArticleList and articles

---
+++ src/components/ArticlesPage/ArticleList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ArticlesPage from "./ArticlesPage";
+
+const articles=[
+    {
+        title:'co',
+        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+    },
+    {
+        title:'co',
+        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+    },
+    {
+        title:'co',
+        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+    },
+    {
+        title:'co',
+        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+    },
+    {
+        title:'co',
+        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+    },
+    {
+        title:'co',
+        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+    },
+]
--- src/components/ArticlesPage/ArticlesPage.jsx
+++ src/components/ArticlesPage/ArticlesPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import Footer from '../common/Footer/Footer'
 import "./ArticlesPage.scss"
 
-export default function ArticlesPage() {
+export default function ArticlesPage(props) {
   return (
     <div className="wrapper">
-      Articles
+     <div className="card">
+       <div className="card__item">
+         <h1>Lorem Ipsum</h1>
+         <p>Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.</p>
+      
+       </div>
+       
+     </div>
     </div>
   )
 }
