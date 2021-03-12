4002fd35ab5e746dc7478185ebdd024389b3dc72
Nazar Archakov <nazar-archakou_student2021@wilder.school>
2021-03-12 22:27:51 +0300

    add footer and header

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -7,17 +7,17 @@ export default function Header() {
   return (
     <div className="footer">
       <div className="footer__nav">
-        <img className="Logo" src={Logo} alt="logo"/>
+        <img className="Logo" src={Logo} alt="logo" />
         <ul className="footer__list">
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/check">Check Air</NavLink></li>
           <li><NavLink to="/articles">Pollution Articles</NavLink></li>
         </ul>
       </div>
-        <div className="createdBy">
-       <p> Created by</p>
-       <p> Wild Code School Students</p>
-        </div>
+      <div className="createdBy">
+        <p> Created by</p>
+        <p> Wild Code School Students</p>
+      </div>
     </div>
   )
 }
