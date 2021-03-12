195b24f37c1bd0602224825487788c0cac8eea7b
gortatka <gtatsiana@tut.by>
2021-03-13 01:47:13 +0300

    add info

--- src/components/ArticlesPage/ArticleList.jsx
+++ src/components/ArticlesPage/ArticleList.jsx
@@ -5,34 +5,40 @@ import './ArticleList.scss';
 const articles=[
     {
         title:'CO',
-        description:'CO is released into the atmosphere as volcanic and bog gases as a result of forest and steppe fires; excreted from the body by microorganisms, plants, animals and people.'
+        description:'CO is released into the atmosphere as volcanic and bog gases as a result of forest and steppe fires; excreted from the body by microorganisms, plants, animals and people.',
+        url:"https://en.wikipedia.org/wiki/Carbon_monoxide",
     },
     {
-        title:'co',
-        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+        title:'NO',
+        description:'Nitrogen dioxide is one of the most common anthropogenic emissions to the atmosphere. It is formed during the course of photochemical reactions of oxides in the atmosphere.',
+        url:"https://en.wikipedia.org/wiki/Nitric_oxide"
     },
     {
-        title:'co',
-        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+        title:'	NO2',
+        description:'Nitrogen dioxide is one of the most common atmospheric pollutants today, playing a significant role in the formation of smog and acid precipitation.',
+        url:"https://en.wikipedia.org/wiki/Nitrogen_dioxide"
     },
     {
-        title:'co',
-        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+        title:'O3',
+        description:'It is a secondary pollutant. It is not emitted into the atmospheric air from sources, but is formed as a result of photochemical reactions of precursors - volatile organic compounds, nitrogen oxides and carbon monoxide, which are emitted from cars, boiler houses and industrial plants.',
+        url:'https://en.wikipedia.org/wiki/Ozone'
     },
     {
-        title:'co',
-        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+        title:'SO2',
+        description:'Sulfur dioxide is a colorless, pungent, poisonous gas. Heavier than air more than twice. It dissolves well in water.',
+        url:'https://en.wikipedia.org/wiki/Sulfur_dioxide'
     },
     {
-        title:'co',
-        description:'Air quality is linked to human activities, earth’s climate and ecosystems globally. Many of the drivers of air pollution (i.e. combustion of fossil fuels) are also sources of CO2.'
+        title:'PM2.5',
+        description:'PM 2.5 is the smallest particles, ranging in size from 0.001 to 2.5 micrometers (μm), found in the air.',
+        url:'https://www.health.ny.gov/environmental/indoors/air/pmq_a.htm'
     }
 ]
 
 const ArticleList=()=>(
     <div className="ArticleList">
         {articles.map(article=>(
-        <ArticlesPage title={article.title} description={article.description}/>
+        <ArticlesPage title={article.title} description={article.description} url={article.url}/>
     ))}
     </div>
 )
