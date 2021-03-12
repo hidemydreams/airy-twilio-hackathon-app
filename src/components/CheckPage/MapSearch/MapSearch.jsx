e2f04a231d069f73551adedf79f7a107508fefd7
Maslovars <maslovars@gmail.com>
2021-03-13 00:57:05 +0300

    add search scss

--- src/components/CheckPage/MapSearch/MapSearch.jsx
+++ src/components/CheckPage/MapSearch/MapSearch.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./MapSearch.scss"
 
 export default function MapSearch({ submitHandler, value, searchChangeHandler }) {
+  const [map, setMap] = useState(false)
   return (
     <div className="mapSearch">
-      <div className="mapSearch__text">
-        <h1>Check air in your city in just 2 clicks!</h1>
-        <p>Enter your city or country to get information about the air you are breathing!</p>
+      <div className={map ? "mapSearch__map-off" : null}>
+        <div className="mapSearch__text">
+          <h1>Check air in your city in just 2 clicks!</h1>
+          <p>Enter your city or country to get information about the air you are breathing!</p>
+        </div>
       </div>
-      <form onSubmit={submitHandler}>
+      <div className={map ? "mapSearch__map-on" : "mapSearch__map-off"}>map</div>
+      <form className="searchForm" onSubmit={submitHandler}>
         <input value={value} onChange={(e) => searchChangeHandler(e)} placeholder="Enter your location" ></input>
-        <div><button>Check</button></div>
+        <div><button onClick={() => setMap(true)} >Check</button></div>
       </form>
     </div>
   )
