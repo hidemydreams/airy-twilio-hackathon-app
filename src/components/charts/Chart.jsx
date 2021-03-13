25c4a1bad322276a5adbdd5f3fc3dcb5d0d348d4
gortatka <gtatsiana@tut.by>
2021-03-13 17:49:12 +0300

    add charts

--- src/components/charts/Chart.jsx
+++ src/components/charts/Chart.jsx
@@ -1,70 +1,65 @@
 import React, {Component} from 'react';
-import {Bar, Line, Pie} from 'react-chartjs-2';
+import {Bar, Pie} from 'react-chartjs-2';
 
 class ChartBar extends Component{
     constructor(props){
       super(props);
       this.state = {
-        chartData:props.chartData
+        chartData:props.chartData,
+        chartData1:props.chartData1,
+        chartData2:props.chartData2,
       }
     }
     static defaultProps = {
-        displayTitle:true,
-        displayLegend: true,
-        legendPosition:'right',
-        location:'City'
-      }
+      displayTitle:true,
     
-      render(){
-        return (
-          <div className="chart">
-            <Bar
-              data={this.state.chartData}
-              options={{
-                title:{
-                  display:this.props.displayTitle,
-                  text:'The most polluted air in '+this.props.location,
-                  fontSize:25
-                },
-                legend:{
-                  display:this.props.displayLegend,
-                  position:this.props.legendPosition
-                }
-              }}
-            />
+      location:'City'
+    }
+  
+    render(){
+      return (
+        <div className="chart">
+          <Bar
+            data={this.state.chartData}
+            options={{
+              title:{
+                display:this.props.displayTitle,
+                text:'Еhe most polluted air in '+this.props.location,
+                fontSize:25
+              },
+              
+            }}
+          />
+  
+          <Bar
+            data={this.state.chartData1}
+            options={{
+              title:{
+                display:this.props.displayTitle,
+                text:'The cleanest air in '+this.props.location,
+                fontSize:25
+              },
+              
+            }}
+          />
+  
+          <Pie
+            data={this.state.chartData2}
+            options={{
+              title:{
+                display:this.props.displayTitle,
+                text:'Human influence on air condition,% ',
+                fontSize:25
+              },
+              
+            }}
+          />
+        </div>
+      )
+    }
+  }
     
-            <Line
-              data={this.state.chartData}
-              options={{
-                title:{
-                  display:this.props.displayTitle,
-                  text:'The most polluted air in'+this.props.location,
-                  fontSize:25
-                },
-                legend:{
-                  display:this.props.displayLegend,
-                  position:this.props.legendPosition
-                }
-              }}
-            />
-    
-            <Pie
-              data={this.state.chartData}
-              options={{
-                title:{
-                  display:this.props.displayTitle,
-                  text:'The most polluted air in '+this.props.location,
-                  fontSize:25
-                },
-                legend:{
-                  display:this.props.displayLegend,
-                  position:this.props.legendPosition
-                }
-              }}
-            />
-          </div>
-        )
-      }}
+     
 
 
 
--- src/components/charts/charts.jsx
+++ src/components/charts/charts.jsx
@@ -5,7 +5,10 @@ class Charts extends Component {
     constructor(){
       super();
       this.state = {
-        chartData:{}
+        chartData:{},
+        chartData1:{},
+        chartData2:{}, 
+
       }
     }
     componentWillMount(){
@@ -15,16 +18,68 @@ class Charts extends Component {
      
         this.setState({
           chartData:{
-            labels: ['Эр-Рияд, Саудовская Аравия', 'Чианг-Май, Тайланд', 'Дакка, Бангладеш', 'Мумбаи, Индия'],
+            labels: ['Mumbai, India', 'Dhaka, Bangladesh','Chiang Mai, Thailand','Beijing, China','Shanghai, China'],
+            datasets:[
+              {
+                label:'PM2.5',
+                data:[
+                  176,
+                  173,
+                  172,
+                  170,
+                  159,
+                  0
+                 ],
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(255, 99, 132, 0.6)'
+                ]
+              }
+            ]
+          },
+          chartData1:{
+            labels: ['New York, USA', 'Tokyo, Japan', 'Melbourne, Australia', 'Bern, Switzerland','Paris, France'],
             datasets:[
               {
                 label:'AQI',
                 data:[
-                  261,
-                  173,
-                  171,
-                  171,
-                                 ],
+                  4,
+                  8,
+                  8,
+                  12,
+                  16,
+                  0
+                 ],
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(255, 99, 132, 0.6)'
+                ]
+              }
+            ]
+          },
+          chartData2:{
+            labels: ['Do not pollute the environment', 'Use environmentally friendly fuels', 'Live a healthy life', 'To plant trees','Use transport less often'],
+            datasets:[
+              {
+                label:'',
+                data:[
+                  47,5,
+                  37,5,
+                  20,
+                  11,25,
+                  21,25,
+                  0
+                 ],
                 backgroundColor:[
                   'rgba(255, 99, 132, 0.6)',
                   'rgba(54, 162, 235, 0.6)',
@@ -39,11 +94,14 @@ class Charts extends Component {
           }
         });
       }
-      render() {
+      render()
+       {
         return (
           <div className="Chart">
             
-            <ChartBar chartData={this.state.chartData} location="Эр-Рияд, Саудовская Аравия" legendPosition="bottom"/>
+            <ChartBar chartData={this.state.chartData}  location="Zabol, Iran" legendPosition="bottom"/>
+            <ChartBar chartData1={this.state.chartData1}  location="New York, USA" legendPosition="bottom"/>
+            <ChartBar chartData2={this.state.chartData2} />
           </div>
         );
       }
